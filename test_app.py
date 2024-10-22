40ce8aba2319e1318f480daa5705254158303bb4
Varun Kumar Gundemoni <Vgundemoni6811@conestogac.on.ca>
2024-10-22 15:55:11 -0400

    added unit tests

---
+++ test_app.py
@@ -0,0 +1,23 @@
+# test_app.py
+
+from app import ToDoList
+
+def test_add_task():
+    todo = ToDoList()
+    todo.add_task("Task 1")
+    assert "Task 1" in todo.tasks
+
+def test_remove_task():
+    todo = ToDoList()
+    todo.add_task("Task 1")
+    todo.remove_task("Task 1")
+    assert "Task 1" not in todo.tasks
+
+def test_show_tasks_empty():
+    todo = ToDoList()
+    assert todo.show_tasks() == "No tasks for today!"
+
+def test_show_tasks_with_items():
+    todo = ToDoList()
+    todo.add_task("Task 1")
+    assert todo.show_tasks() == "Task 1"
